e7604c747a85dbb01594ac74d3f51f761836e006
PrinceHaru397 <kitadeharuki02@gmail.com>
2026-06-08 15:34:14 +0200

    Refactor game2.py to update actor positions and controls

--- game2.py
+++ game2.py
@@ -1,39 +1,33 @@
 import pgzrun
-from pgzero.actor import Actor
-import pylance
-import pygame
+import pgzero.screen
 
 # Window Size
 WIDTH = 800
 HEIGHT = 600
 
 # Actors
-ash = Actor("ash")
-pikachu = Actor("pikachu")
-
-# Setting Initial Positions
-ash.pos = (100, 300)
-pikachu.pos = (650, 300)
+ash1 = Actor("ash", (280, 345))
+pikachu1 = Actor("pikachu", (100, 245))
 
 # Draw Everything
 def draw():
     screen.clear()
-    screen.fill("green")
-    ash.draw()
-    pikachu.draw()
+    screen.fill("skyblue")
+    ash1.draw()
+    pikachu1.draw()
 
 # Keyboard Controls
 def update():
     if keyboard.left:
-        ash.x-=5
+        ash1.x-=5
     if keyboard.right:
-        ash.x+=5
+        ash1.x+=5
     if keyboard.up:
-        ash.y+=5
+        ash1.y-=5
     if keyboard.down:
-        ash.y-=5
-    if ash.colliderect("pikachu"):
-        screen.draw.text("Ash reached Pikachu!", center = (WIDTH//2, 50), fontsize = 40, color = "red")
+        ash1.y+=5
+    if ash1.colliderect(pikachu1):
+        print("ash reached the pikachu!")
 
 # Start
 pgzrun.go()
